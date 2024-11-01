c801871bf652658cf12c0a1fff0abd8a7b251966
Erik <eekihenriksson@gmail.com>
2024-11-01 11:45:58 +0200

    updates

--- slurm_test.sh
+++ slurm_test.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem=16G        
 #SBATCH --cpus-per-task=16         
 #SBATCH --time=00:15:00  
-#SBATCH --output=logs/output_%j.lo
+#SBATCH --output=logs/output_%j.log
 #SBATCH --error=logs/error_%j.log  
 #SBATCH --account=project_2011770
 #SBATCH --partition=gputest
@@ -15,9 +15,9 @@
 module load pytorch/2.4
 
 # Launch each execution of predict.py using one specific GPU
-srun --ntasks=1 --gres=gpu:1 --exclusive python3 predict.py /scratch/project_2011770/webscale-registers/splits/deduplicated/fin_Latn/1/00.jsonl & 
-srun --ntasks=1 --gres=gpu:1 --exclusive python3 predict.py /scratch/project_2011770/webscale-registers/splits/deduplicated/fin_Latn/1/01.jsonl & 
-srun --ntasks=1 --gres=gpu:1 --exclusive python3 predict.py /scratch/project_2011770/webscale-registers/splits/deduplicated/fin_Latn/1/02.jsonl & 
-srun --ntasks=1 --gres=gpu:1 --exclusive python3 predict.py /scratch/project_2011770/webscale-registers/splits/deduplicated/fin_Latn/1/03.jsonl & 
+srun --ntasks=1 --gres=gpu:a100:1 --exclusive python3 predict.py /scratch/project_2011770/webscale-registers/splits/deduplicated/fin_Latn/1/00.jsonl & 
+srun --ntasks=1 --gres=gpu:a100:1 --exclusive python3 predict.py /scratch/project_2011770/webscale-registers/splits/deduplicated/fin_Latn/1/01.jsonl & 
+srun --ntasks=1 --gres=gpu:a100:1 --exclusive python3 predict.py /scratch/project_2011770/webscale-registers/splits/deduplicated/fin_Latn/1/02.jsonl & 
+srun --ntasks=1 --gres=gpu:a100:1 --exclusive python3 predict.py /scratch/project_2011770/webscale-registers/splits/deduplicated/fin_Latn/1/03.jsonl & 
 
 wait  # Wait for all background tasks to complete
