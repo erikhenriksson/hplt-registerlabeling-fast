8b6458a45f39dac651ba803a141ea1d4ee69f74d
Erik <eekihenriksson@gmail.com>
2024-11-27 14:55:08 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -109,8 +109,6 @@ def main(args):
     # id2label = config["id2label"]
     id2label = model.config.id2label
 
-    print(id2label)
-
     output_file = args.output_file
     total_items = 0
     total_time = 0.0
