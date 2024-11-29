96f58bd906c5f4eccbff20df95187aa62b2eaeb9
Erik <eekihenriksson@gmail.com>
2024-11-29 21:37:36 +0200

    Update filter_by_register.py

--- filter_by_register.py
+++ filter_by_register.py
@@ -7,12 +7,64 @@ import zstandard as zstd
 
 from argparse import ArgumentParser
 
-from label_stats import assign_labels
+LABEL_HIERARCHY = {
+    "MT": [],
+    "LY": [],
+    "SP": ["it"],
+    "ID": [],
+    "NA": ["ne", "sr", "nb"],
+    "HI": ["re"],
+    "IN": ["en", "ra", "dtp", "fi", "lt"],
+    "OP": ["rv", "ob", "rs", "av"],
+    "IP": ["ds", "ed"],
+}
+
+LABEL_PARENT = {c: p for p, cs in LABEL_HIERARCHY.items() for c in cs}
+
+
+def is_hybrid(labels):
+    if len(labels) > 2:
+        return True
+    if len(labels) == 2:
+        l1, l2 = labels
+        return not (
+            l1 in LABEL_PARENT
+            and LABEL_PARENT[l1] == l2
+            or l2 in LABEL_PARENT
+            and LABEL_PARENT[l2] == l1
+        )
+    return False
+
+
+def assign_labels(probabilities, threshold):
+    labels = set()
+    for label, prob in probabilities.items():
+        if prob >= threshold:
+            labels.add(label)
+            if label in LABEL_PARENT:
+                # assure that parent also included
+                labels.add(LABEL_PARENT[label])
+    return labels
+
+
+def process(textd, labeld, args):
+    assert textd["id"] == labeld["id"], "id mismatch"
+    probabilities = labeld["register_probabilities"]
+    labels = assign_labels(probabilities, args.threshold)
+
+    if args.no_hybrids and is_hybrid(labels):
+        return
+
+    if labels & args.registers:
+        assert "register_probabilities" not in textd
+        textd["register_probabilities"] = probabilities
+        print(json.dumps(textd, ensure_ascii=False))
 
 
 def argparser():
     ap = ArgumentParser()
     ap.add_argument("--threshold", type=float, default=0.5)
+    ap.add_argument("--exclude_hybrids", action="store_true")
     ap.add_argument("textfile")
     ap.add_argument("labelfile")
     ap.add_argument("registers", help="R1[,R2...]")
@@ -24,16 +76,6 @@ def zopen(fn):
         return zstd.open(fn, "rt")
     else:
         return open(fn)
-
-
-def process(textd, labeld, args):
-    assert textd["id"] == labeld["id"], "id mismatch"
-    probabilities = labeld["register_probabilities"]
-    labels = assign_labels(probabilities, args.threshold)
-    if labels & args.registers:
-        assert "register_probabilities" not in textd
-        textd["register_probabilities"] = probabilities
-        print(json.dumps(textd, ensure_ascii=False))
 
 
 def main(argv):
